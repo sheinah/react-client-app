0c50b808937ce0e80407c2c2b80d71a11a6b136b
Sheniah.CH <104757175+sheinah@users.noreply.github.com>
2024-01-26 17:13:24 +0700

    header design setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,41 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <div className="container-fluid my-container">
+      <div className="my-container container-fluid">
         <div className="row">
-          <div className="col-6 ">
+          <div className="col-4 text-center" style={{ marginTop: 10 }}>
             <h1>Coursite</h1>
           </div>
-          <div className="col-6 ">
-            <h2>Router</h2>
-            {/* <ul className="list-inline">
-              <li className="list-inline-item title">Home</li>
-              <li className="list-inline-item title"></li>
-            </ul> */}
+          <div className="col-5 text-center" style={{ marginTop: 25 }}>
+            <ul className="list-inline">
+              <li className="list-inline-item title mb-2">Home</li>
+              <li className="list-inline-item title mb-2">Services</li>
+              <li className="list-inline-item title mb-2">Courses</li>
+              <li className="list-inline-item title mb-2">Blog</li>
+            </ul>
           </div>
-          <div className="col-6 ">
-            <h3>Button</h3>
+          <div className="col-3 text-right" style={{ marginTop: 20 }}>
+            <button
+              className="btn custom-btn-color title"
+              style={{
+                marginRight: 10,
+                "--bs-btn-padding-y": ".25rem",
+                "--bs-btn-padding-x": ".5rem",
+                "--bs-btn-font-size": ".75rem",
+              }}
+            >
+              Log in
+            </button>
+            <button
+              className="btn custom-btn-color2 title"
+              style={{
+                "--bs-btn-padding-y": ".25rem",
+                "--bs-btn-padding-x": ".5rem",
+                "--bs-btn-font-size": ".75rem",
+              }}
+            >
+              Sign up
+            </button>
           </div>
         </div>
       </div>
